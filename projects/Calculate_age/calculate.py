87964e7b502509d26ada132cef800688795ca789
Aashray24092000 <118126127+Aashray24092000@users.noreply.github.com>
2023-06-30 13:20:29 +0530

    Update calculate.py

--- projects/Calculate_age/calculate.py
+++ projects/Calculate_age/calculate.py
@@ -1,49 +1,12 @@
-# -*- coding: utf-8 -*-
-import time
-from calendar import isleap
+from datetime import date
 
-# judge the leap year
-def judge_leap_year(year):
-    if isleap(year):
-        return True
+def calculate_age(born):
+    today = date.today()
+    try: 
+        birthday = born.replace(year=today.year)
+    except ValueError: # raised when birth date is February 29 and the current year is not a leap year
+        birthday = born.replace(year=today.year, month=born.month+1, day=1)
+    if birthday > today:
+        return today.year - born.year - 1
     else:
-        return False
-
-
-# returns the number of days in each month
-def month_days(month, leap_year):
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
-    elif month in [4, 6, 9, 11]:
-        return 30
-    elif month == 2 and leap_year:
-        return 29
-    elif month == 2 and (not leap_year):
-        return 28
-
-
-name = input("input your name: ")
-age = input("input your age: ")
-localtime = time.localtime(time.time())
-
-year = int(age)
-month = year * 12 + localtime.tm_mon
-day = 0
-
-begin_year = int(localtime.tm_year) - year
-end_year = begin_year + year
-
-# calculate the days
-for y in range(begin_year, end_year):
-    if (judge_leap_year(y)):
-        day = day + 366
-    else:
-        day = day + 365
-
-leap_year = judge_leap_year(localtime.tm_year)
-for m in range(1, localtime.tm_mon):
-    day = day + month_days(m, leap_year)
-
-day = day + localtime.tm_mday
-print("%s's age is %d years or " % (name, year), end="")
-print("%d months or %d days" % (month, day))
+        return today.year - born.year
